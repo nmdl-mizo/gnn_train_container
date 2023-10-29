4a742fa87918e24cf3b7091c8e8e4d7da772eecf
ken2403 <ken2403.pmtdg@gmail.com>
2023-10-29 15:25:42 +0900

    modify db

--- ctx/invar_sphere/train.py
+++ ctx/invar_sphere/train.py
@@ -1,17 +1,14 @@
+from __future__ import annotations
+
 import os
 import sys
 import argparse
 import logging
-import bisect
 import pickle
-from pathlib import Path
 
 import lmdb
-import numpy as np
 import torch
-import torch_geometric
 from torch_geometric.data import Dataset
-from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import CSVLogger, WandbLogger
@@ -30,107 +27,21 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def pyg2_data_transform(data):
-    """
-    if we're on the new pyg (2.0 or later) and if the Data stored is in older format
-    we need to convert the data to the new format
-    """
-    if torch_geometric.__version__ >= "2.0" and "_store" not in data.__dict__:
-        return Data(**{k: v for k, v in data.__dict__.items() if v is not None})
-
-    return data
-
-
 class LmdbDataset(Dataset):
-    r"""Dataset class to load from LMDB files containing relaxation
-    trajectories or single point computations.
-
-    Useful for Structure to Energy & Force (S2EF), Initial State to
-    Relaxed State (IS2RS), and Initial State to Relaxed Energy (IS2RE) tasks.
+    """
+    LMDB dataset.
 
     Args:
-            config (dict): Dataset configuration
-            transform (callable, optional): Data transform function.
-                    (default: :obj:`None`)
+        db_path: Path to the ase database.
     """
 
-    def __init__(self, config, transform=None):
-        super(LmdbDataset, self).__init__()
-        self.config = config
-
-        assert not self.config.get(
-            "train_on_oc20_total_energies", False
-        ), "For training on total energies set dataset=oc22_lmdb"
-
-        self.path = Path(self.config["src"])
-        if not self.path.is_file():
-            db_paths = sorted(self.path.glob("*.lmdb"))
-            assert len(db_paths) > 0, f"No LMDBs found in '{self.path}'"
-
-            self.metadata_path = self.path / "metadata.npz"
-
-            self._keys, self.envs = [], []
-            for db_path in db_paths:
-                self.envs.append(self.connect_db(db_path))
-                length = pickle.loads(self.envs[-1].begin().get("length".encode("ascii")))
-                self._keys.append(list(range(length)))
-
-            keylens = [len(k) for k in self._keys]
-            self._keylen_cumulative = np.cumsum(keylens).tolist()
-            self.num_samples = sum(keylens)
-        else:
-            self.metadata_path = self.path.parent / "metadata.npz"
-            self.env = self.connect_db(self.path)
-            self._keys = [f"{j}".encode("ascii") for j in range(self.env.stat()["entries"])]
-            self.num_samples = len(self._keys)
-
-        # If specified, limit dataset to only a portion of the entire dataset
-        # total_shards: defines total chunks to partition dataset
-        # shard: defines dataset shard to make visible
-        self.sharded = False
-        if "shard" in self.config and "total_shards" in self.config:
-            self.sharded = True
-            self.indices = range(self.num_samples)
-            # split all available indices into 'total_shards' bins
-            self.shards = np.array_split(self.indices, self.config.get("total_shards", 1))
-            # limit each process to see a subset of data based off defined shard
-            self.available_indices = self.shards[self.config.get("shard", 0)]
-            self.num_samples = len(self.available_indices)
-
-        self.transform = transform
-
-    def __len__(self):
-        return self.num_samples
-
-    def __getitem__(self, idx):
-        # if sharding, remap idx to appropriate idx of the sharded set
-        if self.sharded:
-            idx = self.available_indices[idx]
-        if not self.path.is_file():
-            # Figure out which db this should be indexed from.
-            db_idx = bisect.bisect(self._keylen_cumulative, idx)
-            # Extract index of element within that db.
-            el_idx = idx
-            if db_idx != 0:
-                el_idx = idx - self._keylen_cumulative[db_idx - 1]
-            assert el_idx >= 0
-
-            # Return features.
-            datapoint_pickled = self.envs[db_idx].begin().get(f"{self._keys[db_idx][el_idx]}".encode("ascii"))
-            data_object = pyg2_data_transform(pickle.loads(datapoint_pickled))
-            data_object.id = f"{db_idx}_{el_idx}"
-        else:
-            datapoint_pickled = self.env.begin().get(self._keys[idx])
-            data_object = pyg2_data_transform(pickle.loads(datapoint_pickled))
-
-        if self.transform is not None:
-            data_object = self.transform(data_object)
-
-        return data_object
-
-    def connect_db(self, lmdb_path=None):
-        env = lmdb.open(
-            str(lmdb_path),
+    def __init__(
+        self,
+        db_path: str,
+    ):
+        super().__init__(self)
+        self.env = lmdb.open(
+            db_path,
             subdir=False,
             readonly=True,
             lock=False,
@@ -138,14 +49,20 @@ class LmdbDataset(Dataset):
             meminit=False,
             max_readers=1,
         )
-        return env
+        self._keys = [f"{j}".encode("ascii") for j in range(self.env.stat()["entries"] - 1)]
+        self.n_datas = len(self._keys)
 
-    def close_db(self):
-        if not self.path.is_file():
-            for env in self.envs:
-                env.close()
-        else:
-            self.env.close()
+    def close(self) -> None:
+        """Close a database."""
+        self.env.close()
+
+    def len(self):
+        return self.n_datas
+
+    def get(self, idx):
+        data = self.env.begin().get(self._keys[idx])
+        data = pickle.loads(data)
+        return data
 
 
 class DataModule(pl.LightningDataModule):
@@ -272,7 +189,7 @@ def main(args: argparse.Namespace):
 
     # ---------- make dataset ----------
     logger.info("Making dataset...")
-    dataset = LmdbDataset({"src": f"{args.dataset_dir}"})
+    dataset = LmdbDataset(args.dataset_dir)
 
     # split dataset
     max_z = 90
